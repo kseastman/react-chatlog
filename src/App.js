40d32afcd5d50aa4f245357a1b6af76a91a89fc1
kseastman <kat.eastman@gmail.com>
2018-05-29 15:27:16 -0700

    finished wave2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
+
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatLog from './components/ChatLog'
 
 class App extends Component {
   render() {
     console.log(chatMessages);
+    const firstUser = chatMessages[0].sender
+    const secondUser = chatMessages[1].sender
+    const messageData = chatMessages
 
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Application title</h1>
+          <h1 className="App-title">Chat between {firstUser} and {secondUser}</h1>
         </header>
         <main className="App-main">
+          <ChatLog chatLog={messageData} localCheck={firstUser}/>
         </main>
       </div>
     );
--- src/components/Timestamp.js
+++ src/components/Timestamp.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import moment from 'moment';
+import PropTypes from 'prop-types';
 
 class Timestamp extends React.Component {
+  static propTypes = {
+    time: PropTypes.string.isRequired
+  }
+  
   render() {
     const time = moment(this.props.time);
     const absolute = time.format('MMMM Do YYYY, h:mm:ss a');
